7cb49456e152f8520b811bc27fde3aad2979de06
Roman Diakov <dromanua@gmail.com>
2022-02-04 17:14:19 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// $(function () {
+//   $('.top__slider').slick({
+//     arrows: false,
+//     // отключаем кнопки по умолчанию
+//     dots: true,
+//     // включаем точки по умолчанию 
+//     autoplay: true,
+//     // включаем автопроигрывание
+//     fade: true,
+//     // смена картинок через fade
+//   });
+
+//   $('.reviews__slider').slick({
+//     arrows: false,
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1
+//   });
+
+// });
